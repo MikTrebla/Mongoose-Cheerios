d0478b4e57b4d6f4d8b69154879b6b3bff5feac6
Albert Kim <miktrebla92@gmail.com>
2018-07-12 22:23:34 -0700

    port was hard coded

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ var mongoose = require("mongoose");
 
 const app = express();
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.engine('handlebars', exphbs({
     defaultLayout: 'main'
