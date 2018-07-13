5bbfd6518f20d2dc75e67bce2c247dbbe4dd908d
Albert Kim <miktrebla92@gmail.com>
2018-07-12 22:18:03 -0700

    changed port

--- server.js
+++ server.js
@@ -134,6 +134,6 @@ app.post('/note/:id', (req, res) => {
 // })
 
 
-// app.listen(PORT, function () {
-//     console.log('App running on port ' + PORT + '!');
-// });
+app.listen(MONGODB_URI, function () {
+    console.log('App running on port ' + PORT + '!');
+});
